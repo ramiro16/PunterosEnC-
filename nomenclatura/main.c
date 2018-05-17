5fbd3729c67e7326b9933ff83be50dbf3968871c
ramiro16 <ramirotapia16@gmail.com>
2018-05-17 10:24:27 -0300

    primer commit

--- nomenclatura/main.c
+++ nomenclatura/main.c
@@ -3,6 +3,39 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    //punteros
+    int *punteroNumero; //No es un entero es un puntero a entero
+    int numero;
+    int numeroDos;
+
+    numero = 66;
+    numeroDos = &numero;
+
+    //*punteroNumero = &numero;
+    //*punteroNumero = 55;
+
+    //numeroDos = &numero;//asignacion de memoria
+    //printf("\n a- %d",numeroDos);
+
+    //numeroDos = *punteroNumero; //asignacion de valor
+    //printf("\n b- %d",numeroDos);
+
+    printf("\n%d",numero);
+    printf("\n%d",numeroDos);
+    printf("\n%d",&numero);
+    printf("\n%d",punteroNumero);
+    printf("\n%d",&punteroNumero);
+    //printf("\n%d",*punteroNumero);
+    //printf("%d",*punteroNumero);
+
+
+
+    /*printf("\n\n%p",&numero); //%p muestra la direccion del puntero, (en notacion de puntero) en hexadecimal
+    printf("\n%p",punteroNumero);
+    printf("\n%p",&punteroNumero);
+    */
+
+   //SI EL PUNTERO APUNTA A UNA DIRRECION QUE INICIALIZAMOS SIN VALOR PINCHA EL PROGRAMA. SE DEBE INICIALIZAR EN NULL
+
     return 0;
 }
