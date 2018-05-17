498a1ae1cc37dd61386e8c786b33ed504bc68fd6
ramiro16 <ramirotapia16@gmail.com>
2018-05-17 10:37:50 -0300

    Swap de varibles enteras con punteros

---
+++ FuncionesPunteros/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void intercambiar(int num1,int num2); //NO FUNCIONA
+void intercambiarConPunteros(int *num1, int *num2);
+int main()
+{
+    int edadUno;
+    int edadDos;
+    edadUno = 22;
+    edadDos = 99;
+
+    //intercambiar(edadUno,edadDos); //NO FUNCIONA
+    intercambiarConPunteros(&edadUno,&edadDos);
+
+    printf("\nEdad 1:%d ",edadUno);
+    printf("\nEdad 2:%d ",edadDos);
+
+    return 0;
+}
+
+void intercambiar(int num1,int num2)
+{
+    int aux;
+
+    aux = num1;
+    num1 = num2;
+    num2 = aux;
+}
+
+
+void intercambiarConPunteros(int *num1,int *num2)
+{
+    int aux;
+
+    aux = *num1;
+    *num1 = *num2;
+    *num2 = aux;
+}
+
+
+
